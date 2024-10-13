fd9589057ccd047a7b0b9da80661e0914361aa3c
Eduardo Almeida <almeedusa@gmail.com>
2024-10-13 13:24:28 -0300

    Desenvolvimento de lógica para os métodos: CadastrarHospedes() , ObterQuantidadeHospedes() e CalcularValorDiaria()

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            bool validacaoSuite = hospedes.Count <= Suite.Capacidade;
+
+            if (validacaoSuite)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception("Capacidade Máxima Excedida.");
             }
         }
 
@@ -35,23 +34,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+
+            var quantidadePessoas = Hospedes.Count;
+            return quantidadePessoas;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            bool calcularDesconto = DiasReservados >= 10;
+            if (calcularDesconto)
             {
-                valor = 0;
+                decimal desconto = valor * 0.1M;
+                valor -= desconto;
             }
 
             return valor;
